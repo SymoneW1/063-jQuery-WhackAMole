248e411fb6faafc7675949ee2e3bc00d4d668340
SymoneW1 <106252141+SymoneW1@users.noreply.github.com>
2022-05-25 08:11:52 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$("square").click(function(){
+    $(".mole1").hide();
+});
+    
+$("INCORRECT").click(function(){
+    $("INCORRECT").hide();
+});
+    
+$("INCORRECT").click(function(){
+    $("INCORRECT").hide();
+});
+    
+$("INCORRECT").click(function(){
+    $("INCORRECT").show();
+});
